afe1409ebdffab2f3122ddb379c4437759b0cd37
flavio <flavinhosimoesbrito@gmail.com>
2024-09-10 07:56:39 -0400

    Increasing Array

--- IncreasingArray/main.c
+++ IncreasingArray/main.c
@@ -1,44 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Array{
+    int* array;
+    int length;
+} Array;
+
+void increaseArray(Array* array, int newSize);
+void printArray(int* array, int size);
+
+int main(){
+    /*Array arr = {malloc(5 * sizeof(int)), 5};
+    for(int i = 0; i < arr.length; i++)
+    {
+        arr.array[i] = i;
+    }*/
+
+    Array* arr = malloc(sizeof(Array));
+
+    arr->array = malloc(5 * sizeof(int));
+    arr->length = 5;
+
+    for(int i = 0; i < arr->length; i++)
+    {
+        arr->array[i] = i;
+    }
+
+    printArray(arr->array, arr->length);
+
+    increaseArray(arr, 20);
+
+    return 0;
+}
+
+void increaseArray(Array* arr, int newSize){
+    arr->array = realloc(arr->array, newSize * sizeof(int));
+    arr->length = newSize;
+    printArray(arr->array, arr->length);
+    printf("\n");
+}
+
 void printArray(int* array, int size){
     for(int i = 0; i < size; i++)
     {
-        if(i == size - 1)
+        if(i == 0)
         {
             printf("%i", array[i]);
         }
         else
         {
-            printf("%i, ", array[i]);
+            printf(", %i", array[i]);
         }
     }
-    printf("\n");
-}
-
-int main(){
-    int arraySize = 10;
-    int* array = malloc(arraySize * sizeof(int));
-
-    for(int i = 0; i < arraySize; i++)
-    {
-        array[i] = i;
-    }
-
-    printArray(array, arraySize);
-
-    arraySize = 20;
-
-    array = realloc(array, arraySize * sizeof(int));
-
-    printArray(array, arraySize);
-
-    for(int i = 10; i < arraySize; i++)
-    {
-        array[i] = i;
-    }
-
-    printArray(array, arraySize);
-
-    return 0;
+    printf(" ---- print finished\n");
 }
