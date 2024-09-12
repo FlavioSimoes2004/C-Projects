ead8fb2557e881065a9f1d84f659c8ce8aadc8e6
flavio <flavinhosimoesbrito@gmail.com>
2024-09-12 07:32:24 -0400

    hangman game

--- Hangman-Game/main.c
+++ Hangman-Game/main.c
@@ -3,13 +3,46 @@
 #include <string.h>
 #include <time.h>
 
+int repeatedLetter(char* letters, char letter){
+    for(int i = 0; i < strlen(letters); i++)
+    {
+        if(letters[i] == letter)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 //returns the number of different letters in a word
 int getNumLetters(const char* word){
-    int letters = 0;
+    int num_letters = 0;
+    char letters[10];
+    for(int i = 0; i < 10; i++)
+    {
+        letters[i] = '0';
+    }
+    letters[9] = '\0';
 
-    printf("Length word = %i\n", sizeof(word)/sizeof(char));
+    for(int i = 0; i < strlen(word); i++)
+    {
+        if(repeatedLetter(letters, word[i]) == 0)
+        {
+            num_letters = num_letters + 1;
+            //printf("letters array length = %i\n", strlen(letters));
+            for(int j = 0; j < strlen(letters); j++)
+            {
+                if(letters[j] == '0')
+                {
+                    letters[j] = word[i];
+                    //printf("Inserted inside letters, %i.\n", num_letters);
+                    break;
+                }
+            }
+        }
+    }
 
-    return letters;
+    return num_letters;
 }
 
 int main(){
@@ -27,9 +60,13 @@ int main(){
     printf("Length = %i\n", sizeof(words)/sizeof(char*));
     //printf("%s\n", words[0]);
 
+    char test[10];
+    printf("bbbbb%c\n", test[0]);
+
     int choice, chosenWord;
     int num_letters;
     int tries = 10, current_tries = tries;
+    char userChoice;
     do
     {
         printf("0 - Game ends | 1 - Start new game\nChoose: ");
@@ -40,12 +77,44 @@ int main(){
             scanf(" %i", &choice);
         }
 
-        while(choice == 1 && current_tries > 0)
+        if(choice == 1)
         {
             chosenWord = rand() % sizeof(words)/sizeof(char*);
             printf("Word chosen = %s\n", words[chosenWord]);
+
             num_letters = getNumLetters(words[chosenWord]);
-            break;
+            printf("Num letters = %i\n", num_letters);
+
+            char letters_found[strlen(words[chosenWord])+1];
+            letters_found[strlen(words[chosenWord])] = '\0';
+
+            while(1)
+            {
+                //escolhe cor e tals
+                printf("Choose a letter: ");
+                scanf(" %c", &userChoice);
+
+                if(repeatedLetter(words[chosenWord], userChoice) == 0)
+                {
+                    current_tries = current_tries - 1;
+                }
+                else
+                {
+                    num_letters = num_letters - 1;
+                }
+
+                if(num_letters == 0)
+                {
+                    printf("You found the word.\n");
+                    break;
+                }
+                else if(current_tries == 0)
+                {
+                    printf("You lost. The word was: %s.\n", words[chosenWord]);
+                    break;
+                }
+            }
+            current_tries = tries;
         }
     } while (choice != 0);
 
